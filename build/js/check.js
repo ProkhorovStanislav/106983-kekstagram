ebd759c5b6d7edf8027eb5084a33718bdaa3a6fd
ProkhorovStanislav <karser2009@mail.ru>
2016-08-04 21:19:10 +0300

    function fixed

--- build/js/check.js
+++ build/js/check.js
@@ -2,15 +2,20 @@
 
 function getMessage(a, b) {
   var message = 'Функции были переданы некорректные значения';
+  
   if (typeof a === 'boolean') {
     if (a) {
       message = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     } else {
       message = 'Переданное GIF-изображение не анимировано';
     }
-  } else if (typeof a === 'number') {
+  }  
+  
+  if (typeof a === 'number') {
     message = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
-  } else if (Array.isArray(a)) {
+  } 
+
+  if (Array.isArray(a)) {
     if (Array.isArray(b)) {
       var artifactsSquare = 0;
       for (var i = 0; i < a.length; i++) {
@@ -25,11 +30,6 @@ function getMessage(a, b) {
       message = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
     }
   }
+
   return message;
 }
-
-getMessage(true, 3);
-getMessage(false, 4);
-getMessage(5, 4);
-getMessage([1, 2, 3], 4);
-getMessage([1, 2, 3], [2, 3, 4]);
