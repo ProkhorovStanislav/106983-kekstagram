20af25ec59355d054432f297b017ce3106d9fee9
ProkhorovStanislav <karser2009@mail.ru>
2016-08-19 16:51:38 +0300

    (m4-t1) console.log added

--- src/pictures.js
+++ src/pictures.js
@@ -4,14 +4,16 @@
   var pictures = null;
   window.loadPicturesCallback = function(data) {
     pictures = data;
+    console.log(pictures);
   };
+
   function addScriptToMainPage(url, callback) {
     var scriptEl = document.createElement('script');
     scriptEl.src = url + '/?callback=' + callback;
     document.body.appendChild(scriptEl);
   }
 
-  addScriptToMainPage('http://localhost:1506/api/pictures', 'loadPicturesCallback' );
+  addScriptToMainPage('http://localhost:1506/api/pictures', 'loadPicturesCallback');
 
 })();
 
