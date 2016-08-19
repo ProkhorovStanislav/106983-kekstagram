a6140b7df3c01f46c38894f1a229894c1072b8f7
ProkhorovStanislav <karser2009@mail.ru>
2016-08-19 13:09:59 +0300

    (m4-t1) unnecessary lines removed

--- src/pictures.js
+++ src/pictures.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// (function() {
-//
-//   function addScriptToMainPage(url, callbackFunction) {
-//     var scriptEl = document.createElement('script');
-//     scriptEl.src = url + '/?callback=' + callbackFunction;
-//     document.body.appendChild(scriptEl);
-//   }
-//
-//   addScriptToMainPage('http://localhost:1506/api/pictures', 'loadPicturesCallback');
-//   console.log(addScriptToMainPage('http://localhost:1506/api/pictures', 'loadPicturesCallback'));
-// })();
-
 (function() {
 
   function addScriptToMainPage(url, callback) {
@@ -23,9 +11,3 @@
   addScriptToMainPage('http://localhost:1506/api/pictures', 'loadPicturesCallback' );
 
 })();
-
-/**
- Создайте функцию, с помощью которой можно будет выполнять JSONP запросы.
- Функция должна принимать два параметра: один из которых — адрес запроса и второй параметр — функция,
- которая должна вызываться после выполнения JSONP скрипта. Функция, которая была передана вторым параметром,
- должна иметь единственный аргумент: набор данных, который возвращает JSONP-скрипт.*/
