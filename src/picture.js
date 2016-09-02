70836dc1c42fee94df9499b8129e24c9b2232c04
ProkhorovStanislav <karser2009@mail.ru>
2016-09-02 09:39:03 +0300

    (m6-t2) moved out prototypes from Picture

--- src/picture.js
+++ src/picture.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gallery = require('./gallery.js');
-
 var elementToClone;
 var templateElement = document.querySelector('template');
 
@@ -12,46 +11,12 @@ if ('content' in templateElement) {
 }
 
 var Picture = function(response, index) {
-
   var that = this;
   this.element = elementToClone.cloneNode(true);
   var image = this.element.querySelector('img');
 
-  Picture.prototype.setup = function() {
-    this.data = response;
-    var pictureLikes = this.element.querySelector('.picture-likes');
-    var pictureComments = this.element.querySelector('.picture-comments');
-
-    pictureLikes.textContent = this.data.likes;
-    pictureComments.textContent = this.data.comments;
-  };
-
-  Picture.prototype.load = function() {
-    var newImage = new Image();
-
-    newImage.onload = function(evt) {
-      clearTimeout(imageLoadTimeout);
-      image.width = '182';
-      image.height = '182';
-      image.src = evt.target.src;
-    };
-
-    newImage.onerror = function() {
-      that.element.classList.add('picture-load-failure');
-    };
-
-    newImage.src = that.data.url;
-
-    var IMAGE_LOAD_TIMEOUT = 10000;
-
-    var imageLoadTimeout = setTimeout(function() {
-      image.src = '';
-      that.element.classList.add('picture-load-failure');
-    }, IMAGE_LOAD_TIMEOUT);
-  };
-
-  this.setup();
-  this.load();
+  this.setup(response);
+  this.load(image, that);
 
   that.element.onclick = function(event) {
     event.preventDefault();
@@ -61,6 +26,41 @@ var Picture = function(response, index) {
   Picture.prototype.remove = function() {
     that.element.onclick = null;
   };
+};
+
+Picture.prototype = {};
+
+Picture.prototype.setup = function(response) {
+  this.data = response;
+  var pictureLikes = this.element.querySelector('.picture-likes');
+  var pictureComments = this.element.querySelector('.picture-comments');
+
+  pictureLikes.textContent = this.data.likes;
+  pictureComments.textContent = this.data.comments;
+};
+
+Picture.prototype.load = function(image, that) {
+  var newImage = new Image();
+
+  newImage.onload = function(evt) {
+    clearTimeout(imageLoadTimeout);
+    image.width = '182';
+    image.height = '182';
+    image.src = evt.target.src;
+  };
+
+  newImage.onerror = function() {
+    that.element.classList.add('picture-load-failure');
+  };
+
+  newImage.src = this.data.url;
+
+  var IMAGE_LOAD_TIMEOUT = 10000;
+
+  var imageLoadTimeout = setTimeout(function() {
+    image.src = '';
+    that.element.classList.add('picture-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
 };
 
 module.exports = Picture;
