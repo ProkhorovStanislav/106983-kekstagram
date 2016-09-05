c855a8751d60341ec8079c5ad8b764ddf991769e
ProkhorovStanislav <karser2009@mail.ru>
2016-09-05 15:51:41 +0300

    (m7-t1) Main task is done

--- src/load.js
+++ src/load.js
@@ -1,7 +1,16 @@
 'use strict';
 
-module.exports = function addScriptToMainPage(url, callback) {
-  var scriptEl = document.createElement('script');
-  scriptEl.src = url + '/?callback=' + callback;
-  document.body.appendChild(scriptEl);
+var load = function(url, options, callback) {
+  var xhr = new XMLHttpRequest();
+  var param = '?from=' + options.from + '&to=' + options.to + '&filter=' + options.filter;
+  xhr.open('GET', url + param, true);
+  xhr.onload = function() {
+    var jsonData = JSON.parse(xhr.responseText);
+    callback(jsonData);
+
+  };
+
+  xhr.send();
 };
+
+module.exports = load;
--- src/pictures.js
+++ src/pictures.js
@@ -7,17 +7,53 @@
   var pictures = [];
   var picturesContainer = document.querySelector('.pictures');
   var filtersBlock = document.querySelector('.filters');
+  var pageNumber = 0;
+  var PAGESIZE = 12;
+  var dataUrl = 'http://localhost:1506/api/pictures';
 
-  filtersBlock.classList.add('hidden');
+  var loadPicturesNextPage = function() {
+    pageNumber = pageNumber + 1;
+    load(dataUrl, {from: pageNumber * PAGESIZE, to: pageNumber * PAGESIZE + PAGESIZE}, loadPicturesCallback);
+  };
 
-  load('http://localhost:1506/api/pictures', 'loadPicturesCallback');
+  var isBottomReached = function() {
+    var lastImage = picturesContainer.querySelector('.picture:last-child');
+    var positionImage = lastImage.getBoundingClientRect();
+    return positionImage.top - window.innerHeight - 100 <= 0;
+  };
 
-  window.loadPicturesCallback = function(response) {
+  var picturesChange = function() {
+
+    if (isBottomReached()) {
+      setTimeout(loadPicturesNextPage, 300);
+    }
+  };
+
+  var loadPicturesCallback = function(response) {
     pictures = response;
+    if (pageNumber === 0) {
+      picturesContainer.innerHTML = '';
+    }
+    if (pictures.length === 0 || pictures.length < PAGESIZE) {
+      window.addEventListener('scroll', picturesChange);
+    }
     pictures.forEach(function(picture, index) {
       var createPicture = new Picture(picture, index);
       picturesContainer.appendChild(createPicture.element);
     });
     gallery.setPictures(pictures);
   };
+
+  filtersBlock.classList.remove('hidden');
+  load(dataUrl, {from: 0, to: PAGESIZE }, loadPicturesCallback);
+  filtersBlock.addEventListener('change', function(evt) {
+    window.addEventListener('scroll', picturesChange);
+    if (event.target.tagName.toLowerCase() === 'input') {
+      pageNumber = 0;
+      var elementValue = evt.target.value;
+      load(dataUrl, {from: 0, to: PAGESIZE, filter: elementValue}, loadPicturesCallback);
+    }
+  }, true);
+
+  window.addEventListener('scroll', picturesChange);
 })();
