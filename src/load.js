29da28109504d55296a3945eacc30ddf5c761a81
ProkhorovStanislav <karser2009@mail.ru>
2016-09-06 18:54:58 +0300

    (m7-t1) error handlers added to load.js, mistakes corrected, excess scroll handles removed from pictures.js

--- src/load.js
+++ src/load.js
@@ -3,11 +3,23 @@
 var load = function(url, options, callback) {
   var xhr = new XMLHttpRequest();
   var param = '?from=' + options.from + '&to=' + options.to + '&filter=' + options.filter;
+  var filtersBlock = document.querySelector('.filters');
+
   xhr.open('GET', url + param, true);
+
   xhr.onload = function() {
     var jsonData = JSON.parse(xhr.responseText);
     callback(jsonData);
+    filtersBlock.classList.remove('hidden');
+  };
 
+  xhr.onerror = function() {
+    console.warn('Data has not been loaded');
+  };
+
+  xhr.timeout = 0;
+  xhr.ontimeout = function() {
+    console.warn('Response from the server was not received');
   };
 
   xhr.send();
--- src/pictures.js
+++ src/pictures.js
@@ -4,54 +4,53 @@
   var Picture = require('./picture');
   var gallery = require('./gallery');
   var load = require('./load');
-  var pictures = [];
   var picturesContainer = document.querySelector('.pictures');
   var filtersBlock = document.querySelector('.filters');
   var pageNumber = 0;
   var PAGESIZE = 12;
   var dataUrl = 'http://localhost:1506/api/pictures';
+  var scrollTimeout;
 
   var loadPicturesNextPage = function() {
-    pageNumber = pageNumber + 1;
     load(dataUrl, {from: pageNumber * PAGESIZE, to: pageNumber * PAGESIZE + PAGESIZE}, loadPicturesCallback);
   };
 
   var isBottomReached = function() {
     var lastImage = picturesContainer.querySelector('.picture:last-child');
     var positionImage = lastImage.getBoundingClientRect();
-    return positionImage.top - window.innerHeight - 100 <= 0;
+    return positionImage.top - document.documentElement.clientHeight - 100 <= 0;
   };
 
   var picturesChange = function() {
-
-    if (isBottomReached()) {
-      setTimeout(loadPicturesNextPage, 300);
-    }
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(function(){
+      if (isBottomReached()) {
+        pageNumber += 1;
+        loadPicturesNextPage(pageNumber);
+      }
+    }, 100)
   };
 
   var loadPicturesCallback = function(response) {
-    pictures = response;
     if (pageNumber === 0) {
       picturesContainer.innerHTML = '';
     }
-    if (pictures.length === 0 || pictures.length < PAGESIZE) {
-      window.addEventListener('scroll', picturesChange);
-    }
-    pictures.forEach(function(picture, index) {
+
+    response.forEach(function(picture, index) {
       var createPicture = new Picture(picture, index);
       picturesContainer.appendChild(createPicture.element);
     });
-    gallery.setPictures(pictures);
+
+    gallery.setPictures(response);
   };
 
-  filtersBlock.classList.remove('hidden');
-  load(dataUrl, {from: 0, to: PAGESIZE }, loadPicturesCallback);
+  load(dataUrl, {from: pageNumber, to: PAGESIZE}, loadPicturesCallback);
+
   filtersBlock.addEventListener('change', function(evt) {
-    window.addEventListener('scroll', picturesChange);
     if (event.target.tagName.toLowerCase() === 'input') {
       pageNumber = 0;
       var elementValue = evt.target.value;
-      load(dataUrl, {from: 0, to: PAGESIZE, filter: elementValue}, loadPicturesCallback);
+      load(dataUrl, {from: pageNumber, to: PAGESIZE, filter: elementValue}, loadPicturesCallback);
     }
   }, true);
 
